ef7c676ae300d49fb723aa7b03650a60793ed28a
Allison Erin <aerin@calpoly.edu>
2018-02-28 10:55:09 -0800

    update

--- WeightConversion.java
+++ WeightConversion.java
@@ -1,12 +1,19 @@
 
-public class WeightConversion {
+public class WeightConversion 
+{
+	public static void main(String[] args) 
+	{
+        System.out.println("Hello World!"); // Display the string.
+    }
 
-	public double KilosToPounds (double kilos){
+	public double KilosToPounds (double kilos)
+	{
 		
 		return (kilos / 0.454);
 	}
 	
-public double PoundsToKilos (double pounds){
+    public double PoundsToKilos (double pounds)
+    {
 		
 		return (pounds * 0.454);
 	}
